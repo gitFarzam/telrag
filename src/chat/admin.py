696c83344defa1b357470acdc6e562534dd58502
Farzam <farzam.git@gmail.com>
2026-02-09 16:45:43 -0800

    writing document parsing test function

--- src/chat/admin.py
+++ src/chat/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import UserMessage, Conversation
+from .models import UserMessage, Conversation , TelegramMessage , Document
 
+admin.site.register(Document)
 admin.site.register(Conversation)
 admin.site.register(UserMessage)
+admin.site.register(TelegramMessage)
--- src/chat/signals.py
+++ src/chat/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import UserMessage, TelegramMessage
-from .services import process_user_message, process_telegram_message
+from .models import UserMessage, TelegramMessage, Document
+from .services import process_user_message, process_telegram_message, process_document_object
 
 @receiver(post_save, sender=UserMessage)
 def new_user_message_signal(sender, instance, created, **kwargs):
@@ -12,3 +12,14 @@ def new_user_message_signal(sender, instance, created, **kwargs):
 def telegram_message_signal(sender, instance, created, **kwargs):
 	if created:
 		process_telegram_message(instance)
+
+@receiver(post_save, sender=Document)
+def document_parsing_signal(sender, instance, created, **kwargs):
+	if created:
+		process_document_object(instance)
+
+# Source - https://stackoverflow.com/a/18532655
+# Posted by Germano, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-02-09, License - CC BY-SA 3.0
+
+
