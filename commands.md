2ee92ec0211d222b9a5d56367410ac375f82fa34
Farzam <farzam.git@gmail.com>
2026-02-11 09:07:37 -0800

    before changing logic from signals to functions

--- commands.md
+++ commands.md
@@ -1,3 +1,10 @@
 ```shell
 uv run manage.py createsuperuser
 ```
+
+## Tests
+
+```shell
+uv run manage.py test chat.tests.TestTelegramMessageParsing.test_processing_telegram_message
+uv run manage.py test chat.tests.TestDocumentIngestion.test_processing_document
+```
--- src/chat/utils/rag.py
+++ src/chat/utils/rag.py
@@ -2,8 +2,20 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from typing import List
 from langchain_core.documents.base import Document
 
-class TextSplitter(RecursiveCharacterTextSplitter):
+from huggingface_hub import InferenceClient
 
-    def __init__(self, separators = None, keep_separator = True, is_separator_regex = False, text=None,**kwargs):
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class RAGToolKit(RecursiveCharacterTextSplitter):
+
+    def __init__(self, separators = None, keep_separator = True, is_separator_regex = False, text=None, **kwargs):
         super().__init__(separators, keep_separator, is_separator_regex,  chunk_size = 4000,
         chunk_overlap = 200,**kwargs)
+
+
+    def embedder(self,chunks:list):
+        client = InferenceClient(model="sentence-transformers/all-MiniLM-L6-v2" , token="hf_Gd3Gg0o75RfKG3IplnjVKC2tJulngVtKf5") 
+        embedding = client.feature_extraction(text=chunks)
+        print(embedding)
