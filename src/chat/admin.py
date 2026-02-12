8cda87e287ae02c77f263912fa5017e5e7f301cf
Farzam <farzam.git@gmail.com>
2026-02-11 17:41:37 -0800

    fixing issues regarding saving text from telegram

--- src/chat/admin.py
+++ src/chat/admin.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
-from .models import UserMessage, Conversation , TelegramMessage , Document
+from .models import UserMessage, Conversation , TelegramMessage , Document , Chunk, Embedding
 
-admin.site.register(Document)
+
 admin.site.register(Conversation)
 admin.site.register(UserMessage)
 admin.site.register(TelegramMessage)
+admin.site.register(Document)
+admin.site.register(Chunk)
+admin.site.register(Embedding)
