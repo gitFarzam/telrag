357eb88e5bc176328398c8dec227db9564d68a88
Farzam <farzam.git@gmail.com>
2026-03-11 11:07:47 -0700

    dc

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,3 @@ uv run manage.py collectstatic
 echo "📦 Running migrations..."
 uv run manage.py makemigrations
 uv run manage.py migrate
-
-echo "running in a seprated terminal"
-celery -A django_project worker --loglevel=info  
