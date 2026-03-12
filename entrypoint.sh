d256f2e4c97b921a07e72bc1a2a1b30ca99e1421
Farzam <farzam.git@gmail.com>
2026-03-12 10:20:32 -0700

    up

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,11 @@ echo "🧱 Collecting static files..."
 # Ensure we're in the Django project directory where manage.py lives
 cd /usr/src/app/src
 
-uv run manage.py collectstatic --noinput
+python manage.py collectstatic --noinput
 
 echo "📦 Running migrations..."
-uv run manage.py makemigrations
-uv run manage.py migrate
+python manage.py makemigrations
+python manage.py migrate
 
 # Hand off to the container's main command (e.g. daphne, celery)
 exec "$@"
