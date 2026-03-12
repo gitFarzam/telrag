2d0c6809696722f31b2c46d7b49c1893ce53692a
Farzam <farzam.git@gmail.com>
2026-03-12 10:31:36 -0700

    edit setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,9 @@
-docker compose build --no-cache
+echo "Docker | Pruning"
+docker compose down
+docker builder prune --all
+docker image prune -a
+
+echo "Docker | Building"
 chmod +x entrypoint.sh
+docker compose build --no-cache
 docker compose up
