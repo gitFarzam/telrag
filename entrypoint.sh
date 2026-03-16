d99c9ffd0a4a33ee970199ec7a5f577868b9375f
Farzam <farzam.git@gmail.com>
2026-03-16 14:54:57 -0700

    adding dir to entrypint , pwd i mean

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,10 +3,17 @@
 # Exit on error
 set -e
 
+echo "Current Directory"
+pwd
+
 echo "🧱 Collecting static files..."
 
 # Ensure we're in the Django project directory where manage.py lives
 cd /usr/src/app/src
+
+echo "Current Directory After cd"
+pwd
+
 
 python manage.py collectstatic --noinput
 
