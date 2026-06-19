b5318580fa59c5f7dad72539bf8b058fc1a3898d
Farzam <farzam.git@gmail.com>
2026-06-19 16:32:21 -0700

    working on delete inital data command, some editings for removing all iniital data-not working yet

--- src/chat/management/commands/del_inital_data.py
+++ src/chat/management/commands/del_inital_data.py
@@ -1,11 +1,6 @@
-from django.core.management.base import BaseCommand, CommandError
-from chat.models import Conversation,Document,DocumentSource,TextContent
-from django.db import transaction
-from django.conf import settings
-from chat.services import creating_text_content_object, creating_document_source,creating_document_object,creating_chunk_objects,creating_embedding_objects
-import os
-from pathlib import Path
-from django.contrib.contenttypes.models import ContentType
+from django.core.management.base import BaseCommand
+from chat.models import Document,DocumentSource
+
 
 class Command(BaseCommand):
 
@@ -27,5 +22,3 @@ class Command(BaseCommand):
         self.stdout.write(
             self.style.SUCCESS(f"All initial documents are deleted from database")
         )
-
-        # python manage.py del_inital_data
