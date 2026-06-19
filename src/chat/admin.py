ccae88448f569bcbfdbe2384de24f1fa568c4644
Farzam <farzam.git@gmail.com>
2026-06-19 15:22:28 -0700

    adding models into admin panel

--- src/chat/admin.py
+++ src/chat/admin.py
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-from django.contrib import admin
-from .models import Message, Conversation , TelegramMessage , Document , Chunk, Embedding,TelegramChatID
-
-
-admin.site.register(Conversation)
-admin.site.register(Message)
-admin.site.register(TelegramMessage)
-admin.site.register(Document)
-admin.site.register(Chunk)
-admin.site.register(Embedding)
-admin.site.register(TelegramChatID)
-||||||| 6d2c1b6
-=======
 from django.contrib import admin
 from .models import Message, Conversation , TelegramMessage , Document , Chunk, Embedding
 
@@ -22,4 +8,3 @@ admin.site.register(TelegramMessage)
 admin.site.register(Document)
 admin.site.register(Chunk)
 admin.site.register(Embedding)
->>>>>>> demo
