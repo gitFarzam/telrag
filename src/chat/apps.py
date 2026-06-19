70d2f5e7f5dfc13ed355cbfe23dc56832e556881
Farzam <farzam.git@gmail.com>
2026-06-19 15:18:06 -0700

    removing signals.py which was redundant

--- src/chat/apps.py
+++ src/chat/apps.py
@@ -1,14 +1,4 @@
-<<<<<<< HEAD
 from django.apps import AppConfig
 
 class ChatConfig(AppConfig):
     name = 'chat'
-    def ready(self):
-        import chat.signals
-||||||| 6d2c1b6
-=======
-from django.apps import AppConfig
-
-class ChatConfig(AppConfig):
-    name = 'chat'
->>>>>>> demo
