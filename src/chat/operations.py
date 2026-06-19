7850f640c1f9e11da062ddecf0695e7c5117ab49
Farzam <farzam.git@gmail.com>
2026-06-19 15:04:49 -0700

    editing operations file

--- src/chat/operations.py
+++ src/chat/operations.py
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-from .tasks import task_new_message,task_reply_message,task_entities_handling,task_button_handling
-from .utils.telegram import telegram_message_parser
-
-
-def telegram_message_processor(transaction_type:bool , json_content:dict):
-    
-    print(telegram_message_processor.__name__)
-    parsed_data = telegram_message_parser(json_content)
-    print(parsed_data)
-    message_type = parsed_data['type']
-    metadata = parsed_data['metadata']
-    message_data = parsed_data['data']
-    message_id = parsed_data['metadata']['message_id']
-    chat_id = metadata['chat_id']
-    
-    if message_type == 'new':
-        task_new_message.delay(transaction_type,json_content,chat_id,message_id)
-
-    elif message_type == 'reply':
-        task_reply_message.delay(transaction_type,json_content,chat_id,message_id)
-
-    elif message_type == 'entities':
-        task_entities_handling.delay(message_data,chat_id)
-
-    elif message_type == 'button':
-        # Deleting documents button
-        task_button_handling.delay(message_data,chat_id)
-||||||| 6d2c1b6
-=======
 import logging
 
 from .tasks import task_new_message,task_reply_message,task_entities_handling,task_button_handling
@@ -67,4 +37,3 @@ def telegram_message_processor(transaction_type:bool , json_content:dict):
 
 
 
->>>>>>> demo
