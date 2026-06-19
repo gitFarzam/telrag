afae0d0633142ec49ee92b28c17779928c016999
Farzam <farzam.git@gmail.com>
2026-06-19 16:38:47 -0700

    chaning celery.py file, removing the parts which were related to demo mode

--- src/django_project/celery.py
+++ src/django_project/celery.py
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
-import os
-from celery import Celery
-
-# Set the default Django settings module for the 'celery' program.
-os.environ.setdefault("DJANGO_SETTINGS_MODULE","django_project.settings")
-
-celery = Celery('django_project')
-
-celery.config_from_object("django.conf:settings",namespace="CELERY")
-celery.autodiscover_tasks()
-||||||| 6d2c1b6
-=======
 import os
 from celery import Celery
 
@@ -34,10 +21,3 @@ celery.conf.update(
     worker_log_format='[%(asctime)s: %(levelname)s/%(processName)s] %(message)s',
     worker_task_log_format='[%(asctime)s: %(levelname)s/%(processName)s] %(task_name)s: %(message)s',
 )
-
-# Add task time limits (prevents freezing) - we already did this in settings.py
-# celery.conf.update(
-#     task_time_limit=300,        # hard limit
-#     task_soft_time_limit=240,   # graceful stop
-# )
->>>>>>> demo
