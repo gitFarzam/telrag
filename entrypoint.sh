92dcf468cf41ff77f2db3b4bfa722479da1e3fec
Farzam <farzam.git@gmail.com>
2026-06-19 16:42:56 -0700

    recomitting entrypoint, because of some issues in merging demo and main branches

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,38 +1,3 @@
-<<<<<<< HEAD
-#!/bin/sh
-
-# Exit on error
-set -e
-
-echo "-> ls"
-ls -a
-
-echo "Current Directory"
-pwd
-
-echo "🧱 Collecting static files..."
-
-# Ensure we're in the Django project directory where manage.py lives
-# Change to directory where manage.py is
-cd /usr/src/app/src || exit 1
-
-echo "Current Directory After cd"
-pwd
-
-echo "-> ls"
-ls -a
-
-python manage.py collectstatic --noinput
-
-echo "📦 Running migrations..."
-python manage.py makemigrations
-python manage.py migrate
-
-echo "finighsing entrypint"
-
-exec "$@"  # executes CMD from Dockerfile or Compose
-||||||| 6d2c1b6
-=======
 #!/bin/sh
 
 # Exit on error
@@ -55,4 +20,3 @@ echo "Finishing entrypint execution"
 
 
 exec "$@"  # executes CMD from Dockerfile or Compose
->>>>>>> demo
